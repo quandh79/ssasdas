07806113591c2dfd606197eaa9f69e58eac1bbae
quandh79 <dohongquantc@gmail.com>
2022-10-13 20:00:22 +0700

    68

--- src/library/HomeController.java
+++ src/library/HomeController.java
@@ -13,5 +13,13 @@ public class HomeController {
         Main.rootStage.setScene(new Scene(listBook, 800, 600));
     }
 
+    public void goToStudentList(javafx.event.ActionEvent actionEvent) throws Exception{
+        Parent listBook = FXMLLoader.load(getClass().getResource("students/list/sutdentlist.fxml"));
+        Main.rootStage.setTitle("Students");
+        Main.rootStage.setScene(new Scene(listBook, 800, 600));
+    }
+
+
+
 
 }
--- src/library/books/list/BookListController.java
+++ src/library/books/list/BookListController.java
@@ -2,13 +2,21 @@ package library.books.list;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import library.Main;
 import library.entities.Book;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class BookListController implements Initializable {
@@ -18,6 +26,11 @@ public class BookListController implements Initializable {
     public TableColumn<Book,String> tdAuthor;
     public TableColumn<Book,Integer> tdQty;
 
+
+    public final static String connecttionString = "jdbc:mysql://localhost:3306/t2203e";
+    public final static String user = "root";
+    public final static  String pwd = ""; // xampp: "", mamp: "root"
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tdId.setCellValueFactory(new PropertyValueFactory<Book,Integer>("id"));
@@ -25,9 +38,36 @@ public class BookListController implements Initializable {
         tdAuthor.setCellValueFactory(new PropertyValueFactory<Book,String>("author"));
         tdQty.setCellValueFactory(new PropertyValueFactory<Book,Integer>("qty"));
         ObservableList<Book> ls = FXCollections.observableArrayList();
-        ls.add(new Book(1, "tri do thai", "Abc", 10));
-        ls.add(new Book(2, "tri ", "Ac", 11));
-        tbBook.setItems(ls);
+       // lay data from database
 
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection c = DriverManager.getConnection(connecttionString,user,pwd);
+            Statement statement = c.createStatement();
+            String sql_txt = "select * from books";
+            ResultSet rs = statement.executeQuery(sql_txt);
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String author = rs.getString("author");
+                int qty = rs.getInt("qty");
+                Book b = new Book(id, name, author,qty);
+                ls.add(b);
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            tbBook.setItems(ls);
+        }
+
+
+
+    }
+
+    public void back(javafx.event.ActionEvent actionEvent) throws Exception{
+        Parent listBook = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Main.rootStage.setTitle("Books");
+        Main.rootStage.setScene(new Scene(listBook, 800, 600));
     }
 }
